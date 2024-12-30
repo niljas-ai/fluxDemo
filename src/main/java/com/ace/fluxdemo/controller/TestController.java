1749128648356d611041a1deccae5e61225ab89a
niljas-ai <sunilpanda56783@gmail.com>
2024-12-30 11:00:26 +0530

    initial commit

---
+++ src/main/java/com/ace/fluxdemo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.ace.fluxdemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import javax.management.monitor.MonitorNotification;
+
+@RestController
+public class TestController {
+
+
+
+
+    @GetMapping("/test")
+    public Mono<ResponseEntity<String>> testController(){
+
+        return Mono.justOrEmpty(ResponseEntity.ok("Successfull"));
+    }
+}
